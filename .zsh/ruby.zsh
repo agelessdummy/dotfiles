b306f56a37e6c130373f8fe3d54eeba0737a97ec
agelessdummy <mendez.angulo@gmail.com>
2018-08-11 18:05:06 -0700

    added Ruby programming stuff

---
+++ .zsh/ruby.zsh
@@ -0,0 +1,7 @@
+# Ruby stuff
+# -------------------------------------------------------------------
+alias ri='ri -Tf ansi' # Search Ruby documentation
+alias rake="noglob rake" # necessary to make rake work inside of zsh
+alias be='bundle exec'
+alias bx='bundle exec'
+alias gentags='ctags .'
