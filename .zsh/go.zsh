b082143819a6e37f0b19907db2096431b0ab080c
agelessdummy <mendez.angulo@gmail.com>
2018-08-23 15:29:24 -0700

    changed ~/.zsh/go.zsh

--- .zsh/go.zsh
+++ .zsh/go.zsh
@@ -1,4 +1,5 @@
-export PATH=$PATH:/usr/lib/go/bin 
-
-export GOROOT=$HOME/go1.X
+#export PATH=$PATH:/usr/local/go/bin
+#export GOROOT=$HOME/go
+export GOROOT=/usr/lib/go
 export PATH=$PATH:$GOROOT/bin
+#export GOPATH=$HOME/go
